e2b57d87f2cb301d464a356a1ff8b6a4c4469b25
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-01-17 15:13:26 +0100

    initial commit

---
+++ Assets/buttonListener.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonListener : MonoBehaviour
+{
+    private Button button => GetComponent<Button>();
+    // Start is called before the first frame update
+    void Start()
+    { // used instead of inspector On Click event for better searchability in vs code
+        button.onClick.AddListener(() =>
+        {
+            PromptInput.Instance.CreateEntryFromPrompt();
+        });
+    }
+}
