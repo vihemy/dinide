b1f9c198323428b92decb15f21a09eb6d36cdb33
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-01-19 10:01:39 +0100

    deleted useless comment

--- Assets/scripts/buttonListener.cs
+++ Assets/scripts/buttonListener.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class ButtonListener : MonoBehaviour
 {
     private Button button => GetComponent<Button>();
-    // Start is called before the first frame update
+
     void Start()
     { // used instead of inspector On Click event for better searchability in vs code
         button.onClick.AddListener(() =>
