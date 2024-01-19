850d592e98661163dccaff404242702f883574e5
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-01-19 10:51:49 +0100

    remove oldest entries

--- Assets/Scripts/EntryCache.cs
+++ Assets/Scripts/EntryCache.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EntryCache : Singleton<EntryCache>
 {
     public List<EntryData> entries = new List<EntryData>();
+    public int maxEntries = 10; // Set your desired maximum size
 
     public void AddEntry(EntryData entry)
     {
+        if (entries.Count >= maxEntries)
+        {
+            entries.RemoveAt(0); // Remove the oldest entry
+        }
         entries.Add(entry);
-        SlideshowController.Instance.UpdateAndDisplayNewEntry(entry);
+
+        SlideshowController.Instance.UpdateAndDisplayNewEntry(entry); // Notify SlideshowController about the new entry
     }
 
     public void RemoveEntry(EntryData entry)
