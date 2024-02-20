177ae24ddbd8074f9e58068d2ab5d4717d67f792
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-02-20 14:08:43 +0100

    pulled StartGamecall from insepctor to listenerscript

---
+++ Assets/Scripts/ui/StartButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    private Button button => GetComponent<Button>();
+    void Start()
+    { // used instead of inspector On Click event for better searchability in vs code
+        button.onClick.AddListener(() =>
+        {
+            OnButtonPress();
+        });
+    }
+
+    private void OnButtonPress()
+    {
+        GameManager.Instance.OnStartGame();
+    }
+}
