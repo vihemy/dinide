472aa77567bfbf34a3012cdec8b32e4b01c404b9
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-02-20 15:29:10 +0100

    Add animation files and update DashboardManager.cs

--- Assets/Scripts/DashboardManager.cs
+++ Assets/Scripts/DashboardManager.cs
@@ -8,6 +8,8 @@ using TMPro;
 public class DashboardManager : Singleton<DashboardManager>
 {
     [SerializeField] private TextMeshProUGUI statusText;
+    [SerializeField] private Animator animator1;
+    [SerializeField] private Animator animator2;
 
     public void IdleDashBoard()
     {
@@ -17,10 +19,20 @@ public class DashboardManager : Singleton<DashboardManager>
     public void StartDashBoard()
     {
         statusText.text = "Behandler idé";
+        animator1.SetTrigger("Start");
+        animator1.ResetTrigger("Stop");
+
+        animator2.SetTrigger("Start");
+        animator2.ResetTrigger("Stop");
     }
 
     public void StopDashBoard()
     {
         statusText.text = "Behandling færdig";
+        animator1.SetTrigger("Stop");
+        animator1.ResetTrigger("Start");
+
+        animator2.SetTrigger("Stop");
+        animator2.ResetTrigger("Start");
     }
 }
