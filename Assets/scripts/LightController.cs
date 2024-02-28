fdda895f2a11c0e966080aa353373d24fd4af9c4
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-02-28 12:32:28 +0100

    Simplify

--- Assets/scripts/LightController.cs
+++ Assets/scripts/LightController.cs
@@ -6,25 +6,29 @@ using UnityEngine.Rendering.Universal;
 [System.Serializable]
 public class LightData
 {
-    public Light2D light;
+    public SpriteRenderer spriteRenderer;
     public float minIntensity = 0.0f;
     public float maxIntensity = 1.0f;
-
 }
 
 public class LightController : Singleton<LightController>
 {
     public LightData[] lightsData;
-
-    [Header("SpriteRenderer")]
-    public SpriteRenderer spriteRenderer;
-    public float minAlpha = 0.0f;
-    public float maxAlpha = 1.0f;
     [Header("Flash")]
     public float intensityFactor = 1.0f; // Factor to scale the intensity of all lights
-    [SerializeField] public float flashDuration = 0.5f; 
+    [SerializeField] public float flashDuration = 0.5f;
 
     public event Action OnFlashHalfway;
+
+    public float flashDelay = 1.0f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Flash();
+        }
+    }
     public void Flash()
     {
         StartCoroutine(FlashRoutine());
@@ -39,7 +43,7 @@ public class LightController : Singleton<LightController>
             timer += Time.deltaTime;
             float lerpFactor = timer / (flashDuration / 2);
 
-            UpdateLightsAndSprite(lerpFactor);
+            UpdateSpriteAlpha(lerpFactor);
 
             yield return null;
         }
@@ -51,22 +55,44 @@ public class LightController : Singleton<LightController>
             timer += Time.deltaTime;
             float lerpFactor = 1 - (timer / (flashDuration / 2));
 
-            UpdateLightsAndSprite(lerpFactor);
+            UpdateSpriteAlpha(lerpFactor);
 
             yield return null;
         }
     }
 
-    private void UpdateLightsAndSprite(float lerpFactor)
+    private void UpdateSpriteAlpha(float lerpFactor)
     {
-        foreach (var lightData in lightsData)
+        for (int i = 0; i < 2; i++)
         {
-            lightData.light.intensity = Mathf.Lerp(lightData.minIntensity, lightData.maxIntensity, lerpFactor);
+            var lightData = lightsData[i];
+            float alpha = Mathf.Lerp(lightData.minIntensity, lightData.maxIntensity, lerpFactor);
+            Color spriteColor = lightData.spriteRenderer.color;
+            spriteColor.a = alpha;
+            lightData.spriteRenderer.color = spriteColor;
         }
 
-        float alpha = Mathf.Lerp(minAlpha, maxAlpha, lerpFactor);
-        Color spriteColor = spriteRenderer.color;
-        spriteColor.a = alpha;
-        spriteRenderer.color = spriteColor;
+        // StartCoroutine(FadeLastLightData(flashDelay));
+
+    }
+
+    // Fade last LightData-object with a delay
+    private IEnumerator FadeLastLightData(float delay)
+    {
+
+        LightData lastLightData = lightsData[lightsData.Length - 1];
+        float timer = 0.0f;
+        while (timer < flashDuration - delay)
+        {
+            timer += Time.deltaTime;
+            float lerpFactor = timer / flashDuration;
+
+            float alpha = Mathf.Lerp(lastLightData.maxIntensity, lastLightData.minIntensity, lerpFactor);
+            Color spriteColor = lastLightData.spriteRenderer.color;
+            spriteColor.a = alpha;
+            lastLightData.spriteRenderer.color = spriteColor;
+
+            yield return null;
+        }
     }
 }
