da59593912d31f1727f5eb872a97552f9fe581f1
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-02-28 12:32:48 +0100

    Added AnimateGlow

--- Assets/Scripts/ui/ButtonListener.cs
+++ Assets/Scripts/ui/ButtonListener.cs
@@ -6,6 +6,9 @@ using UltimateClean;
 public class ButtonListener : MonoBehaviour
 {
     private CleanButton button => GetComponent<CleanButton>();
+    [SerializeField] private Image glow;
+    [SerializeField] private float glowMaxAlpha = 1f;
+    [SerializeField] private float glowSpeed = 1f;
 
     void Start()
     { // used instead of inspector On Click event for better searchability in vs code
@@ -19,11 +22,35 @@ public class ButtonListener : MonoBehaviour
 
     public void OnButtonPress()
     {
+
         GameManager.Instance.SubmitIdea();
     }
 
     public void MakeInteractable(bool state)
     {
         button.interactable = state;
+
+        if (state)
+        {
+            StartCoroutine(AnimateGlow());
+        }
+        else
+        {
+            StopAllCoroutines();
+            glow.color = new Color(glow.color.r, glow.color.g, glow.color.b, 0f);
+        }
     }
+
+    IEnumerator AnimateGlow()
+    {
+        glow.color = new Color(glow.color.r, glow.color.g, glow.color.b, 0);
+        while (true)
+        {
+            float t = Mathf.PingPong(Time.time * glowSpeed, 1f);
+            float alpha = Mathf.Lerp(0, glowMaxAlpha, t);
+            glow.color = new Color(glow.color.r, glow.color.g, glow.color.b, alpha);
+            yield return null;
+        }
+    }
+
 }
