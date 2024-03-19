d683ea236b8a08480a910d103482dd8cd87c9610
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-03-19 12:28:12 +0100

    Add checkmark icon to OutputState

--- Assets/Scripts/FSM/OutputState.cs
+++ Assets/Scripts/FSM/OutputState.cs
@@ -7,6 +7,7 @@ public class OutputState : IState
     public void Enter()
     {
         Logger.Instance.Log("Entering Output State");
+        UIManager.Instance.SetPanelIcon(UIManager.PanelIcon.Checkmark);
     }
     public void Execute() { /* Logic to update during the idle state */ }
     public void Exit()
